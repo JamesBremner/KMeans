c21b16c5854fb126324c2201c317d72c522d9cbb
James Bremner <james@ravenspoint.com>
2019-08-19 11:56:26 -0400

    code documentation

--- src/cRow.h
+++ src/cRow.h
@@ -3,7 +3,7 @@
 #include <cmath>
 #include <algorithm>
 
-/// Data row
+/// Data row with variable number of columns
 struct cRow
 {
     int myDim;
@@ -19,53 +19,63 @@ struct cRow
         The dimensionality will be set to the length of vd
     */
     cRow(const std::vector< double >& vd )
-    : cRow( vd.size() )
+        : cRow( vd.size() )
     {
         for( int k = 0; k < myDim; k++ )
             d[k] = vd[k];
     }
+
+    /// Add row
     cRow operator + ( cRow b )
     {
         cRow r( myDim );
-        std::transform( d.begin(), d.end(), b.d.begin(), r.d.begin(),
-                        []( double a, double b)
+        std::transform(
+            d.begin(), d.end(), b.d.begin(), r.d.begin(),
+            []( double a, double b)
         {
             return a+b;
         } );
         return r;
     }
 
+    /// Subtract row
     cRow operator - (cRow b)
     {
         cRow r( myDim );
-        std::transform( d.begin(), d.end(), b.d.begin(), r.d.begin(),
-                        []( double a, double b)
+        std::transform(
+            d.begin(), d.end(), b.d.begin(), r.d.begin(),
+            []( double a, double b)
         {
             return a-b;
         } );
         return r;
     }
+    /// Divide row by scalar
     cRow operator / ( double s )
     {
         if( std::fabs(s) < 0.00000001 )
             throw std::runtime_error("cRow divide by zero");
 
         cRow r( myDim );
-        std::transform( d.begin(), d.end(), r.d.begin(),
-                        [&s]( double a )
+        std::transform(
+            d.begin(), d.end(), r.d.begin(),
+            [&s]( double a )
         {
             return a / s;
         } );
         return r;
 
     }
+
+    /// Pythagonal distance to another row.
     double dist( const cRow& o )
     {
         //cout << "dist " << Text() << " " << o.Text() << "\n";
         double t = 0;
         cRow r( myDim );
-        std::transform( d.begin(), d.end(), o.d.begin(), r.d.begin(),
-                        [&t]( double a, double b)
+        std::transform(
+            d.begin(), d.end(), o.d.begin(), r.d.begin(),
+            [&t]( double a, double b)
         {
             double delta = a-b;
             t += delta * delta;
@@ -74,6 +84,11 @@ struct cRow
 
         return std::sqrt( t );
     }
+
+    /** Sum dirtance to other rows
+        @param[in] locs the other rows
+        @return total distance
+    */
     double sum_all_distances( const std::vector<cRow>& locs )
     {
         double total = 0;
@@ -83,6 +98,8 @@ struct cRow
         }
         return total;
     }
+
+    /// Human readable string to display data
     std::string Text() const
     {
         std::stringstream ss;
