756f897dbb53e230113f36b11adde806d1100174
James Bremner <james@ravenspoint.com>
2020-02-08 16:35:19 -0500

    use STL to calculate distance

--- src/DistanceCalculator.cpp
+++ src/DistanceCalculator.cpp
@@ -6,25 +6,46 @@
 
 #define PYTHAGORUS
 
+#include <vector>
+#include <numeric>
 #include "cRow.h"
+
+using namespace std;
 
 double dist( const cRow& ra, const cRow& rb )
 {
 #ifdef PYTHAGORUS
     //cout << "dist " << ra.Text() << " " << rb.Text() << "\n";
-    double t = 0;
+//    double t = 0;
+//
+//    // loop over data dimensions
+//    for( int k = 0; k < ra.myDim; k++ )
+//    {
+//        // difference between data points for this dimension
+//        double delta = ra.d[k] - rb.d[k];
+//
+//        // accumulate squares of differences
+//        t += delta * delta;
+//    }
+//
+//    // return square of sum of differences squared
+//    return std::sqrt( t );
 
-    // loop over data dimensions
-    for( int k = 0; k < ra.myDim; k++ )
+    vector< double > squared_deltas( ra.myDim );
+    transform(
+        ra.d.begin(), ra.d.end(),
+        rb.d.begin(),
+        squared_deltas.begin(),
+        [](double a, double b)
     {
-        // difference between data points for this dimension
-        double delta = ra.d[k] - rb.d[k];
+        double delta = a - b;
+        return delta * delta;
+    });
+    return sqrt(
+               accumulate(
+                   squared_deltas.begin(),
+                   squared_deltas.end(),
+                   0 ));
 
-        // accumulate squares of differences
-        t += delta * delta;
-    }
-
-    // return square of sum of differences squared
-    return std::sqrt( t );
 #endif // PYTHAGORUS
 }
